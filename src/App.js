16c7781747e5a83df2d39c1c723836fbe6d18516
bosungkim <ghalsru@naver.com>
2020-11-22 23:41:23 +0900

    half-processed class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// react 는 자동으로 render 함수를 실행한다.
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  // axios 에서 데이터를 가져오는데 시간이 걸리기 때문에 async , await 를 써준다.
+getMovies = async () => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+  this.setState({ movies , isLoading: false })
+}
+
+  componentDidMount(){
+    this.getMovies();
+
+  }
+  // 상태를 변경하게되면 render함수를 다시 호출해야되는데, setState 를 사용하면 자동으로 변경해준다.
+
+  render(){
+    const { isLoading, movies } = this.state;
+    return <div>
+              {isLoading ? "Loading..." : movies.map(movie => {
+                return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+              })}
+          </div>
+  }
+
 }
 
 export default App;
